b40ea78f8eadfedfc7caafbf1b0a560d6e249d05
jackharrop <83253747+jackharrop@users.noreply.github.com>
2022-05-12 22:47:28 +1000

    Dodge
    Implemented a dodge roll animation

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -15,7 +15,10 @@ public class PlayerMovement : MonoBehaviour
     public GameObject[] HealthImage;
     public Sprite[] HealthSprite;
     private int damage = 0;
-    private int MaxHealth = 3; //Will implement if i add a item to increase max health
+    private float Timer = 0.0f;
+    private bool Isdodging = false;
+    public BoxCollider2D GroundTouchingBoxcollider;
+    //private int MaxHealth = 3; //Will implement if i add a item to increase max health
     // Start is called before the first frame update
     void Start()
     {
@@ -35,25 +38,57 @@ public class PlayerMovement : MonoBehaviour
 
     private void Movement()
     {
+        float movement = Sidetoside();
+
+        Dodging(movement);
+        Fliping(movement);
+
+    }
+
+    private float Sidetoside()
+    {
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * speed;
         animator.SetFloat("Speed", Mathf.Abs(movement));
+        return movement;
+    }
 
-        if (Input.GetButtonDown("Dodge"))
-        {
-            _rigidbody.velocity = new Vector2((movement * speed) * DodgeDistance, _rigidbody.velocity.y);
-        }
-       
-     
-        
-        
+    private void Fliping(float movement)
+    {
         if (movement > 0)
         {
-            gameObject.transform.localScale = new Vector3(4 ,4,4 );
+            gameObject.transform.localScale = new Vector3(4, 4, 4);
         }
         if (movement < 0)
         {
             gameObject.transform.localScale = new Vector3(-4, 4, 4);
+        }
+    }
+
+    private void Dodging(float movement)
+    {
+        if (Input.GetButtonDown("Dodge"))
+        {
+            animator.SetBool(("IsDodging"), true);
+            Isdodging = true;
+            _rigidbody.velocity = new Vector2((movement * speed) * DodgeDistance, _rigidbody.velocity.y);
+
+
+
+        }
+        if (Isdodging == true)
+        {
+            Timer += Time.deltaTime;
+            if (Timer > 1.5f)
+            {
+                animator.SetBool(("IsDodging"), false);
+                Isdodging = false;
+                Timer = 0.0f;
+            }
+            else
+            {
+                Timer += Time.deltaTime;
+            }
         }
     }
 
@@ -86,9 +121,9 @@ public class PlayerMovement : MonoBehaviour
             HealthImage[damage].gameObject.GetComponent<Image>().sprite = HealthSprite[0];
             damage = damage - 1;
         }
-        if (other.gameObject.CompareTag("MoreHealth")) //Will implement if i add a item to increase max health
-        {
+        //if (other.gameObject.CompareTag("MoreHealth")) //Will implement if i add a item to increase max health
+        //{
 
-        }
+       // }
     }
 }
